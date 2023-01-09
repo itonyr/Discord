9e9dabe80cba2e64c536eee28868f2a03abde8c7
Toner <67766491+itonyr@users.noreply.github.com>
2023-01-09 01:32:43 -0500

    refactor messages module to work as a class

--- src/apps/discord-message-sender/modules/messages/messages.py
+++ src/apps/discord-message-sender/modules/messages/messages.py
@@ -2,22 +2,28 @@ import requests
 import logging
 
 # Class for all message interactions leveraging the Discord webhooks
-class Messages: 
-    # Method for sending messages to a channel using the Webhook_URL
-    def send(self,
-             # Webhook URL. Retrieved in the Settings > Integrations > Webhooks 
-             # field within a given Discord Server. 
-             webhook_url: str,
-             message_content: str
-             ): 
+class message:
+    def __init__(self,webhook_url: str,message_content: str): 
+        logging.info("Message class invoked",)
+        self.webhook_url = webhook_url
+        self.message_content = message_content
     ## Import the webhook URL from Hashicorp Vault
+    def send(self):
         try:
             data = {
-                "content": message_content
+                "content": self.message_content
             }
-            response = requests.post(webhook_url, json=data)
-            logging.info(f"Response recieved from webhook: {response.status_code}")
-            return response
+            response = requests.post(self.webhook_url, json=data)
+            logging.info(f"Response recieved from webhook:")
         except:
             logging.error('Unable to send webhook.')
     # Method for sending messages to a channel using the Webhook_URL
+
+# if __name__ == "__main__":
+#     print("What would you like to do? \f 1: Send Message")
+#     selection = input("")
+#     match selection: 
+#         case 1: 
+#             Messages.send(webhook_url=input("webhook_url"), 
+#                 message_content=input("Message_Content")
+#             )
